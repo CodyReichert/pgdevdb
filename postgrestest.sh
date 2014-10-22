0e4aefdc1d36789543f89d5735898bd07abc573b
Cody Reichert <cody@reichertbrothers.com>
2014-10-22 00:26:49 -0500

    added '-d' option for removed created db's and users s

--- postgrestest.sh
+++ postgrestest.sh
@@ -1,8 +1,17 @@
 #!bin/bash
 
+while getopts ":d:" OPT; do
+    case $OPT in
+        d)
+	    sudo -u postgres dropdb $OPTARG
+	    sudo -u postgres dropuser $OPTARG
+	    echo "Postgres user and database dropped."
+	    exit
+            ;;
+    esac
+done
+
 sudo su postgres <<EOF
 psql -c "CREATE USER $1 WITH PASSWORD '$1';"
-echo "User $1 created!"
 createdb -O$1 -Eutf8 $1 
-echo "Database $1 created!"
-
+echo "Postgres user and database '$1' created."
